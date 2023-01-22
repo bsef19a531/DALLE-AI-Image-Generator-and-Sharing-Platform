7069039db34b564ba7d7c9a303a6e3c85ff31291
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2023-01-22 16:28:41 +0500

    Added Util random prompt func

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+impo
 
 const CreatePost = () => {
     return (
