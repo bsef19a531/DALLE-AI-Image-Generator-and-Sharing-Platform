f2963dc40434fef00637cc468c64ed77aafe46ce
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2023-01-23 01:20:39 +0500

    Implemented Download Function

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,5 @@
 import { surpriseMePrompts } from "../constants";
+import FileSaver from 'file-saver';
 
 export function getRandomPrompt(prompt) {
     const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -10,5 +11,5 @@ export function getRandomPrompt(prompt) {
 }
 
 export async function downloadImage(_id, photo) {
-    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+    FileSaver.saveAs(photo, `download-${_id}-PALLE-AI.jpg`);
 }
